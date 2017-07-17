85b80587031a838771a98be1818bfa2ea961ea98
poonamphate <poonamphate07@gmail.com>
2017-07-17 15:59:47 -0700

    Rotating array inplace

---
+++ RotateArrayDemo2/src/RotateArrayDemo2.java
@@ -0,0 +1,29 @@
+/*Rotate an array of n elements to the right by k steps without using arraycopy()*/
+import java.util.*;
+
+public class RotateArrayDemo2 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();//length of array
+		int k = scanner.nextInt();
+		
+		int[] arr = new int[n];
+		
+		for(int i = 0; i<n; i++){
+			arr[i] = scanner.nextInt();
+		}
+		
+		int temp[] = new int[n];
+		for(int i = 0; i< k; i++){
+			for(int j = n-1; j>0; j-- ){
+				temp[j] = arr[j];
+				arr[j] = arr[j-1];
+				arr[j - 1] = temp[j];
+			}
+		}
+		
+		System.out.println("rotated array : "+Arrays.toString(arr));
+	}
+
+}
