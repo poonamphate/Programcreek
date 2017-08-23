c5746fa62ed465dd9090bc3a55d8e5c9f36610cc
poonamphate <poonamphate07@gmail.com>
2017-08-22 17:28:02 -0700

    added comment

--- isomorphicString/src/isomorphicString/isomorphicString.java
+++ isomorphicString/src/isomorphicString/isomorphicString.java
@@ -1,3 +1,7 @@
+/*Given two strings s and t, determine if they are isomorphic. Two strings are isomorphic if the characters in s can be replaced to get t.
+
+For example,"egg" and "add" are isomorphic, "foo" and "bar" are not.*/
+
 package isomorphicString;
 
 import java.util.HashMap;
